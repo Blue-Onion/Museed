0cc427df8b46e10629e2aa486116f6a04152ca09
Blue Onion <rawatadityasingh963@gmail.com>
2025-07-23 09:33:41 +0530

    feat: add new UI components including Drawer, Dropdown Menu, Hover Card, Input, Navigation Menu, Popover, Progress, Radio Group, Select, Separator, Sheet, Skeleton, Sonner, Switch, Tabs, Textarea, and Tooltip
    - Implemented Drawer component with various subcomponents for enhanced UI interactions.
    - Created Dropdown Menu with support for items, groups, and checkboxes.
    - Added Hover Card for displaying additional information on hover.
    - Developed Input and Textarea components with customizable styles.
    - Introduced Navigation Menu for structured navigation with dropdown capabilities.
    - Implemented Popover for contextual information display.
    - Added Progress component for visual representation of progress.
    - Created Radio Group and Radio Group Item for selection options.
    - Developed Select component with various functionalities including scroll buttons and separators.
    - Introduced Separator for visual separation of content.
    - Implemented Sheet component for modal-like interactions.
    - Added Skeleton for loading states.
    - Integrated Sonner for toast notifications.
    - Developed Switch component for toggle functionality.
    - Created Tabs component for tabbed navigation.
    - Implemented Tooltip for additional context on hover.

--- app/page.js
+++ app/page.js
@@ -1,14 +1,33 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="container. mx-auto">
       <div className="text-center space-y-2 text-white mt-20 px-4">
-        <h1 className="text-7xl md:text-8xl font-bold gradient-title mb-3">Musseed</h1>
-        <h2 className="text-5xl md:text-6xl font-semibold gradient-title mb-4">Your Soundtrack to Feelings.</h2>
-        <p className="text-2xl  text-white">
+        <h1 className="text-5xl md:text-7xl lg:text-8xl gradient-title mb-3">Musseed</h1>
+
+
+        <h2 className="text-3xl md:text-5xl lg:text-6xl gradient-title mb-4">Your Soundtrack to Feelings.</h2>
+        <p className="lg:text-2xl text-muted-foreground">
           Document what you listen to, track how it makes you feel, and turn moments into melodies.
         </p>
+        <div className="flex mt-6 justify-center items-center gap-4">
+          <Link href={"/dashboard"}>
+            <Button>
+              Get Started
+            </Button>
+          </Link>
+          <Link href={"/dashboard"}>
+            <Button variant={"secondary"}>
+              Learn More
+            </Button>
+          </Link>
+        </div>
+        <section className="my-3">
+
+        </section>
       </div>
 
 
